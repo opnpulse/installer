a32118271557051f1acb1635722ae244a764c438
Rokibul Hasan <mdrokibulhasan@appscode.com>
2026-07-15 18:29:05 +0600

    Use service-gateway tls (#13)
    Signed-off-by: Rokibul Hasan <mdrokibulhasan@appscode.com>

--- apis/installer/v1alpha1/register.go
+++ apis/installer/v1alpha1/register.go
@@ -53,7 +53,8 @@ func Resource(resource string) schema.GroupResource {
 
 // Adds the list of known types to api.Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
 		&AppscodeOtelStack{},
 		&AppscodeOtelStackList{},
 		&InboxAgent{},
@@ -72,7 +73,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ThanosOperatorList{},
 	)
 
-	scheme.AddKnownTypes(SchemeGroupVersion,
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
 		&metav1.Status{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
--- apis/installer/v1alpha1/types_test.go
+++ apis/installer/v1alpha1/types_test.go
@@ -26,7 +26,8 @@ import (
 )
 
 func TestDefaultValues(t *testing.T) {
-	checker := schemachecker.New(os.DirFS("../../.."),
+	checker := schemachecker.New(
+		os.DirFS("../../.."),
 		schemachecker.TestCase{Obj: v1alpha1.AppscodeOtelStackSpec{}},
 		schemachecker.TestCase{Obj: v1alpha1.InboxAgentSpec{}},
 		schemachecker.TestCase{Obj: v1alpha1.InboxServerDistributedSpec{}},
